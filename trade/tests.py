d42934cc0a22455425dc5c4d1c5689fc37e878d6
bard <xx61509xx@gmail.com>
2024-12-05 14:49:12 +0800

    🚧 WIP: 建置中

--- trade/tests.py
+++ trade/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_animals_can_speak(self):
+        self.assertEqual('aa', 'aa')
+
+    def test_bb(self):
+        # 需要以test_開頭
+        self.assertEqual('aa', 'bb')
+
+
+# 測試指令
+# python manage.py test
